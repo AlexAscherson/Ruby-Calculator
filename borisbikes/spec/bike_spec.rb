ab3ee07c4ddd4af05df958aad701f38b93ff7e9f
MacBook <MacBook@MacBooks-MacBook-Pro.local>
2015-05-21 16:36:14 +0100

    refactored test conditions

--- borisbikes/spec/bike_spec.rb
+++ borisbikes/spec/bike_spec.rb
@@ -2,22 +2,29 @@ require_relative '../lib/bike.rb'
 
 #We're describing the functionality of a specific class, Bike.
 describe Bike do
+  let(:bike) { Bike.new }
    #this is a specific feature of behavior
    #that we EXPECT to have
  it 'should not be broken when created' do
-    bike = Bike.new #instantiate a new instance of the Bike class
+  # we added the let at the top so dont need to make a bike every time anymore.
+    # bike = Bike.new #instantiate a new instance of the Bike class
     expect(bike.broken?).to be false
    #  or 
    #  expect(bike.broken).not_to be true
  end
 
  it 'should be able to break' do
-    bike = Bike.new
+    # bike = Bike.new
     bike.break
     expect(bike.broken?).to be true
   end
 
-
+ it 'should be able to be fixed' do
+  # bike = Bike.new
+  bike.break
+  bike.fix
+  expect(bike.broken?).to be false
+  end
 
 
 
