89daf08eed15b3a6e28f8809d0bfa43f2d3d04e5
MacBook <MacBook@MacBooks-MacBook-Pro.local>
2015-05-21 16:22:38 +0100

    two conditions passing, added bike class with broken status

--- borisbikes/lib/bike.rb
+++ borisbikes/lib/bike.rb
@@ -1,6 +1,18 @@
 class Bike
 
-  def broken?
-    false
+  def initialize
+    @broken = false
+
+    # the @ means its an instance variable, this means that it only referes to the 
+    # one bike thats been created/selected, not all of them
   end
+
+  def broken?
+    @broken
+  end
+
+  def break
+    @broken = true
+  end
+
 end
--- borisbikes/spec/bike_spec.rb
+++ borisbikes/spec/bike_spec.rb
@@ -2,14 +2,22 @@ require_relative '../lib/bike.rb'
 
 #We're describing the functionality of a specific class, Bike.
 describe Bike do
- #this is a specific feature of behavior
- #that we EXPECT to have
+   #this is a specific feature of behavior
+   #that we EXPECT to have
  it 'should not be broken when created' do
-  bike = Bike.new #instantiate a new instance of the Bike class
-  expect(bike.broken?).to be false
- #  or 
- #  expect(bike.broken).not_to be true
+    bike = Bike.new #instantiate a new instance of the Bike class
+    expect(bike.broken?).to be false
+   #  or 
+   #  expect(bike.broken).not_to be true
  end
+
+ it 'should be able to break' do
+    bike = Bike.new
+    bike.break
+    expect(bike.broken?).to be true
+  end
+
+
 
 
 
